83611533f05480f6206995aea0fca9db0d79036c
vaniashows <vania6600@gmail.com>
2018-03-01 16:36:48 +0300

    alot

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let textLeft;
+let textDone;
+
 let prevInput;
 let isMistake = false;
 let prevMistakeState = false;
@@ -6,6 +8,8 @@ let mistakeCount = 0;
 
 let start;
 let end;
+
+let formStyle;
 
 function getText() {
     let request = new XMLHttpRequest();
@@ -16,9 +20,9 @@ function getText() {
             let text = data.texts[Math.floor(Math.random() * data.texts.length)];
 
             if (text) {
-                document.getElementById("text-to-input").innerText = text.text;
+                document.getElementById("text").innerText = text.text;
             } else {
-                document.getElementById("text-to-input").innerText = "Something went wrong. Please, reload the page.";
+                document.getElementById("text").innerText = "Something went wrong. Please, reload the page.";
             }
         }
     };
@@ -29,37 +33,53 @@ function getText() {
 function checkInput(input) {
     if (!input) {
         isMistake = false;
-        document.body.style.backgroundColor = "#ffffff";
+        // document.body.style.backgroundColor = "#ffffff";
+        formStyle.border = "2px solid #686868";
     }
     for (let i = 0; i < input.length; i++) {
         if (input[i] !== textLeft[i]) {
             isMistake = true;
-            document.body.style.backgroundColor = "#f8b6b2";
+            // document.body.style.backgroundColor = "#f8b6b2";
+            formStyle.border = "2px solid #f8b6b2";
         }
+    }
+}
+
+function updateTextToInput() {
+    let textToInput = document.getElementById("text");
+    if (textDone) {
+        textToInput.innerHTML = "<span id='correct'>" + textDone + " " + "</span>" + textLeft;
     }
 }
 
 function checkWords(input) {
     if (!isMistake) {
-        if (input === textLeft) {
-            end = new Date();
-            calculateStats();
-            document.body.style.backgroundColor = "#c9f8cc";
-            textLeft = "";
-            document.forms["myForm"]["form"].disabled = true;
-        }
-
         for (let i = 0; i < input.length - 1; i++) {
             if (input[i] !== textLeft[i]) {
-                document.body.style.backgroundColor = "#f8b6b2";
+                // document.body.style.backgroundColor = "#f8b6b2";
+                formStyle.border = "2px solid #f8b6b2";
                 isMistake = true;
                 return;
             }
         }
 
+        if (input === textLeft) {
+            end = new Date();
+            calculateStats();
+            formStyle.border = "2px solid #c9f8cc";
+            textLeft = "";
+            document.forms["myForm"]["form"].disabled = true;
+
+            let textToInput = document.getElementById("text");
+            textToInput.innerHTML = "";
+            document.getElementById("correct").innerText = textToInput.innerText;
+        }
+
         textLeft = textLeft.substr(input.length);
-        // console.log(textLeft);
         document.forms["myForm"]["form"].value = "";
+
+        textDone = document.getElementById("text").innerText;
+        textDone = textDone.slice(0, -(textLeft.length + 1));
     }
 }
 
@@ -71,7 +91,8 @@ function controlErase(input) {
                 return;
             }
         }
-        document.body.style.backgroundColor = "#ffffff";
+        // document.body.style.backgroundColor = "#ffffff";
+        formStyle.border = "2px solid #686868";
         isMistake = false;
     }
 }
@@ -81,15 +102,21 @@ function confirmText() {
     if (input === textLeft) {
         end = new Date();
         calculateStats();
-        document.body.style.backgroundColor = "#c9f8cc";
+        // document.body.style.backgroundColor = "#c9f8cc";
+        formStyle.border = "2px solid #c9f8cc";
         document.forms["myForm"]["form"].value = "";
         document.forms["myForm"]["form"].disabled = true;
+
+        let textToInput = document.getElementById("text");
+        textToInput.innerHTML = "<span id='correct'>" + textToInput.innerText + "</span>";
     }
 
     return false;
 }
 
 function formInput() {
+    formStyle = document.getElementById("type-input").style;
+
     //fix unprintable symbols
     let input = document.forms["myForm"]["form"].value;
     input.replace(/«/g, '"');
@@ -98,14 +125,14 @@ function formInput() {
 
     //init
     if (!textLeft) {
-        textLeft = document.getElementById("text-to-input").innerText;
+        textLeft = document.getElementById("text").innerText;
     }
     if (!prevInput && input) {
         prevInput = input;
     }
 
     //time count
-    if (input.length === 1 && textLeft === document.getElementById("text-to-input").innerText) {
+    if (input.length === 1 && textLeft === document.getElementById("text").innerText) {
         start = new Date();
     }
 
@@ -119,15 +146,16 @@ function formInput() {
         if (prevInput.length - input.length === 1) {
             controlErase(prevInput, textLeft);
         }
-        if (input === textLeft || !textLeft) {
-            document.forms["myForm"]["form"].value = "";
-        }
-        if (input === textLeft || !textLeft) {
+        if (input === textLeft + " " || !textLeft) {
             end = new Date();
             calculateStats();
-            document.body.style.backgroundColor = "#c9f8cc";
+            // document.body.style.backgroundColor = "#c9f8cc";
+            formStyle.border = "2px solid #c9f8cc";
             document.forms["myForm"]["form"].value = "";
             document.forms["myForm"]["form"].disabled = true;
+            
+            let textToInput = document.getElementById("text");
+            textToInput.innerHTML = "<span id='correct'>" + textToInput.innerText + "</span>";
         }
     }
 
@@ -136,6 +164,8 @@ function formInput() {
         document.getElementById("mistake-counter").textContent = "Mistakes: " + mistakeCount.toString();
     }
 
+    updateTextToInput();
+
     prevMistakeState = isMistake;
     prevInput = input;
 }
@@ -143,7 +173,7 @@ function formInput() {
 function calculateStats() {
     let timeElapsed = (end - start) / 60000;
 
-    let initText = document.getElementById("text-to-input").innerText;
+    let initText = document.getElementById("text").innerText;
     let wordsCount = initText.split(' ').length;
     document.getElementById("wpm").innerText = "WPM: " + (wordsCount / timeElapsed).toFixed(0);
 }
